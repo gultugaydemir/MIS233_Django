01e4d08a4b499657a0dba0e0f8b95166b517b68c
Zeynep Gültuğ Aydemir <gultugaydemir@windowslive.com>
2021-01-14 01:22:45 +0300

    models classes created

--- dotabase/models.py
+++ dotabase/models.py
@@ -10,3 +10,41 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Hero(models.Model):
+    name = models.CharField(max_length=50)
+    isPassive = models.BinaryField()
+    description = models.CharField(max_length=200)
+    team = models.CharField(max_length=30) #radiand or dire
+    primary_attribute = models.CharField(max_length=50) #agi int or str
+    attack_type = models.CharField(max_length=10) #ranged or melee
+    roles = models.CharField(max_length=200) #carry, support, escape, ganker, etc.
+    abilities = {
+        "abilityName" : models.CharField(max_length=100),
+        "description" : models.CharField(max_length=300)
+    }
+    baseAgility = models.IntegerField()
+    baseStrength = models.IntegerField()
+    baseIntelligence = models.IntegerField()
+    AgilityGain = models.FloatField()
+    StrengthGain = models.FloatField()
+    IntelligenceGain = models.FloatField()
+    ArmorPhysical = models.FloatField()
+    StatusHealth = models.IntegerField()
+    StatusHealthRegen = models.FloatField()
+    StatusMana = models.IntegerField()
+    StatusManaRegen = models.FloatField()
+    AttackDamage = models.IntegerField()
+    AttackRange = models.IntegerField()
+    Complexity = models.IntegerField()
+    MovementSpeed = models.IntegerField()
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    attack_type = models.CharField(max_length=10) #ranged or melee
+    roles = models.CharField(max_length=200) #carry, support, escape, ganker, etc.
+    price = models.IntegerField()
+    
+    
