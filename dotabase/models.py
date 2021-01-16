f423f3aa5451d47fa0379779807fac806827e7b0
Sedat <sedat.genc@boun.edu.tr>
2021-01-16 18:02:17 +0300

    changes in html files

--- dotabase/models.py
+++ dotabase/models.py
@@ -6,6 +6,7 @@ class Heroes(models.Model):
     strength = models.CharField(max_length=3)
     agility = models.CharField(max_length=3)
     intelligence = models.CharField(max_length=3)
+    image = models.URLField(default="https://static.wikia.nocookie.net/dota2_gamepedia/images/7/7a/Strength_attribute_symbol.png/revision/latest/scale-to-width-down/40?cb=20180323111829", max_length=300)
 
     def __str__(self):
         return self.name
